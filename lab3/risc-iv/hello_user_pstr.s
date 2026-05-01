7169ccb729c0872dcb412ce04d42121cfaca3b6e
TheGeniusOfEternity <max06safin@yandex.ru>
2026-05-01 16:51:30 +0300

    refactor(lab3|risc-iv): store greeting in buffer

--- lab3/risc-iv/hello_user_pstr.s
+++ lab3/risc-iv/hello_user_pstr.s
@@ -15,22 +15,24 @@
      ; t3 - name size counter
      ; t4 - name size limit
 
+     ; t5 - max greeting size
+
      ; ra - store programm counter
 
 
     .data
+
+greeting:        .byte  '?Hello, '
+question:        .byte  'What is your name?\n'
 
 overflow_value:  .word  0xFFFFFFFF
 
 input_addr:      .word  0x80
 output_addr:     .word  0x84
 
-question:        .byte  'What is your name?\n'
-greeting:        .byte  'Hello, '
-
     .text
 
-    .org     0x100
+    .org 0x100
 
 _start:
     lui      a0, %hi(input_addr)             ; load the upper 20 bits of input_addr address
@@ -41,28 +43,36 @@ _start:
     addi     a1, a1, %lo(output_addr)        ; load the lower 12 bits of output_addr address & add them to previous 20
     lw       a1, 0(a1)                       ; load value from output_addr (0x84) to a0 register
 
-write_question:
-    addi     t0, zero, question              ; set ptr to question start
     addi     t2, zero, 0x0A                  ; set '\n' as  stop symbol
-
-    jal      ra, write_symbol_loop           ; call write_symbol_loop procedure
-    sb       t2, 0(a1)                       ; write '\n' symbol to output
-
-read_name:
     addi     t3, zero, 0x00                  ; reset name size counter to 0x00
     addi     t4, zero, 0x17                  ; set max name size
+    addi     t5, zero, 0x20                  ; set max greeting size
 
-    addi     t0, zero, greeting              ; set ptr to greeting start
-    addi     t0, t0, 0x07                    ; move ptr to greeting end
+write_question:
+    addi     t0, zero, question              ; set ptr to question start
+
+    jal      ra, write_symbol_loop           ; call write_symbol_loop procedure
+
+read_name:
+    addi     t0, zero, greeting              ; set ptr to buffer start
+    addi     t0, t0, 0x08                    ; move ptr to skip first part of the greeting
 
     jal      ra, read_symbol_loop            ; call read_symbol_loop procedure
 
+    addi     t2, zero, 0x21                  ; set stop symbol as '!'
+    sb       t2, 0(t0)                       ; write current symbol to buffer
+
+fill_buffer:
+    addi     t0, t0, 1                       ; increment ptr after '!'
+    addi     t1, zero, 0x5F                  ; set current symbol as '_'
+
+    jal      ra, fill_buffer_loop            ; call fill_buffer_loop procedure
+
 write_greeting:
     addi     t0, zero, greeting              ; set ptr to greeting start
-    jal      ra, write_symbol_loop           ; call write_symbol_loop procedure
+    addi     t0, t0, 0x01                    ; increment ptr to skip greeting size byte
 
-    addi     t1, zero, 0x21                  ; set current symbol as '!'
-    sb       t1, 0(a1)                       ; write current symbol to output
+    jal      ra, write_symbol_loop           ; call write_symbol_loop procedure
 
 finish:
     halt                                     ; stop the program
@@ -77,27 +87,40 @@ overflow:
 
     ; ------- Procedures --------
 
+    ; ------- Read symbol -------
+
 read_symbol_loop:
     lb       t1, 0(a0)                       ; load current symbol from input
-    sb       t1, 0(t0)                       ; write current symbol to buffer
 
+    beq      t1, t2, read_symbol_loop_end    ; compare current symbol with stop symbol, if equal then stop reading
+
+    sb       t1, 0(t0)                       ; write current symbol to buffer
     addi     t0, t0, 1                       ; increment ptr
     addi     t3, t3, 1                       ; increment name counter
 
-    bne      t1, t2, read_symbol_loop        ; compare current symbol with stop symbol, if not equal, continue reading
+    j        read_symbol_loop                ; goto read_symbol_loop
 
-    addi     t3, t3, -1                      ; decrement name counter (last symbol, '\n' is not related to name)
+read_symbol_loop_end:
     bgt      t3, t4, overflow                ; compare read count with limit, if limit is less then goto overflow
-
     jr       ra                              ; return to pс stored in ra
+
+    ; ------- Write symbol -------
 
 write_symbol_loop:
     lb       t1, 0(t0)                       ; load current symbol from data by ptr
+    sb       t1, 0(a1)                       ; write current symbol to output
     addi     t0, t0, 1                       ; increment ptr
 
-    beq      t1, t2, write_symbol_loop_end   ; compare current symbol with stop symbol, if equal then stop writing
-    sb       t1, 0(a1)                       ; write current symbol to output
-    j        write_symbol_loop               ; goto write_symbol_loop
+    bne      t1, t2, write_symbol_loop       ; compare current symbol with stop symbol, if not equal then continue writing
+    jr       ra                              ; return to pc stored in ra
 
-write_symbol_loop_end:
+    ; ------- Fill buffer -------
+
+fill_buffer_loop:
+    beq      t0, t5, fill_buffer_loop_end    ; compare ptr with buffer size, if equal then stop filling
+    sb       t1, 0(t0)                       ; write current symbol to buffer by ptr
+    addi     t0, t0, 0x01                    ; increment ptr
+    j        fill_buffer_loop                ; goto fill_buffer_loop
+
+fill_buffer_loop_end:
     jr       ra                              ; return to pc stored in ra
